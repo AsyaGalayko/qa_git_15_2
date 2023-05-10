76be31994150fa9ba44e0d543d718e448e00f08f
Asya <cat.s_heart@mail.ru>
2023-05-11 00:29:14 +0400

    Добавление файлов

---
+++ src/test/java/ru/catsheart/Example3.java
@@ -0,0 +1,11 @@
+package ru.catsheart;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Example3 {
+    @Test
+    void exampleTest () {
+        Assertions.assertTrue(2 > 1);
+    }
+}
